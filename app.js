fd7429064cd1d163388ceaccc3a37af59d811d0e
NimitBedi <nimit.493@gmail.com>
2016-11-28 20:38:37 +0530

    removing credentials

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ var mongoose = require('mongoose');
 var config = require('./config');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var client = require('twilio')('AC14e6e781ac71d95e371a849369ae48f1','632122e2975bce6a8a66ea48de33ee4c');
+var client = require('twilio')('','');
 var cloudinary = require('cloudinary');
 var multer = require('multer');
 
 cloudinary.config({
-	cloud_name : 'dx4smoqgc',
-	api_key: '691337517113189',
-	api_secret : 'yqyyu8NefIekkAVNPwV790PZFX4'
+	cloud_name : '',
+	api_key: '',
+	api_secret : ''
 });
 
 
@@ -28,11 +28,7 @@ db.once('open',function(){
 
 
 var routes = require('./routes/index');
-//var users = require('./routes/users');
-//var dishRouter = require('./routes/dishes');
-//var promotions = require('./routes/promotions');
 var godSellers = require('./routes/godsellers');
-//var pictures = require('./routes/pictures');
 var app = express();
 
 // view engine setup
@@ -46,18 +42,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//passport config
-/*var User = require('./models/user');
-app.use(passport.initialize());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());*/
-
-/*var vendor = require('./models/vendor');
-app.use(passport.initialize());
-passport.use(new LocalStrategy(vendor.authenticate()));
-passport.serializeUser(vendor.serializeUser());
-passport.deserializeUser(vendor.deserializeUser());*/
 
 var admin = require('./models/admin');
 app.use(passport.initialize());
@@ -65,20 +49,11 @@ passport.use(new LocalStrategy(admin.authenticate()));
 passport.serializeUser(admin.serializeUser());
 passport.deserializeUser(admin.deserializeUser());
 
-/*var customer = require('./models/customer');
-app.use(passport.initialize());
-passport.use(new LocalStrategy(customer.authenticate()));
-passport.serializeUser(customer.serializeUser());
-passport.deserializeUser(customer.deserializeUser());*/
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-//app.use('/users', users);
-//app.use('/dishes',dishRouter);
-//app.use('/promotions',promotions);
 app.use('/godsellers',godSellers);
-//app.use('/pictures',pictures);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
